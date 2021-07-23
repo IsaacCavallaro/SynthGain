733df7a216c7cb02a4a3da99b14371910419a812
isaaccavallaro <isaaccavallaro@gmail.com>
2021-07-23 12:58:37 +1000

    Separate many to many relationship with joining table feature_listings

--- app/models/feature.rb
+++ app/models/feature.rb
@@ -1,2 +1,5 @@
 class Feature < ApplicationRecord
+    has_many :feature_listings
+    #Separate many to many relationship with joining table feature_listings
+    has_many :listings, through: :feature_listings
 end
--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,4 +1,7 @@
 class Listing < ApplicationRecord
   belongs_to :category
   enum condition: { needs_repair: 0, used: 1, like_new: 2 }
+  has_many :feature_listings
+  #Separate many to many relationship with joining table feature_listings
+  has_many :listings, through: :feature_listings
 end
