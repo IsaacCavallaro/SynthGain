b73ee1c442140c32906cdd3f8454c1055efd23a5
isaaccavallaro <isaaccavallaro@gmail.com>
2021-07-23 13:24:37 +1000

    adding checkbox relationship to listing model

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -3,5 +3,8 @@ class Listing < ApplicationRecord
   enum condition: { needs_repair: 0, used: 1, like_new: 2 }
   has_many :feature_listings
   #Separate many to many relationship with joining table feature_listings
-  has_many :listings, through: :feature_listings
+  has_many :features, through: :feature_listings
+
+  #Checkboxes 
+  accepts_nested_attributes_for :feature_listings
 end
