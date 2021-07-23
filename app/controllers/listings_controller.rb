1e7740c0c444a7449a138c7b210bf8fd9acdf50c
isaaccavallaro <isaaccavallaro@gmail.com>
2021-07-23 13:55:54 +1000

    Add permission for feature ids

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -75,6 +75,6 @@ class ListingsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def listing_params
-      params.require(:listing).permit(:title, :description, :price, :availability, :condition, :category_id)
+      params.require(:listing).permit(:title, :description, :price, :availability, :condition, :category_id, feature_ids: [])
     end
 end
