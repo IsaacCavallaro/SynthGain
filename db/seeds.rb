b9ec174a370f87069d4d0b142fac3936ff9c698f
isaaccavallaro <isaaccavallaro@gmail.com>
2021-07-23 10:39:50 +1000

    Migrated files and seeded database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,31 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+#Set the categories for the category entity
+categories = ["Polyphonic", "Monophonic", "Modular"]
+
+#Set the features for the feature entity
+features = ["Weighted Keys", "Arpeggiator", "Vocodor"]
+
+#Seed database if the category count is 0
+if Category.count == 0
+    #iterate through categories
+    categories.each do |category|
+        #Create a title and description for each category
+        Category.create(title: category, description: "This is a test description")
+        #print to console
+        puts "Created #{category} category"
+    end
+end
+
+#Seed database if the feature count is 0
+if Feature.count == 0
+    #iterate through features
+    features.each do |feature|
+        #Create a title for features
+        Feature.create(title: feature)
+        #print to console
+        puts "Created #{feature} feature"
+    end
+end
