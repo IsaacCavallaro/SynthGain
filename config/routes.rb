82acc8a72c8667987986ea2a024d48579a162cf3
isaaccavallaro <isaaccavallaro@gmail.com>
2021-07-26 14:59:35 +1000

    Created about synthgain page and route

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get 'payments/webhook'
   devise_for :users
   root to: 'pages#home'
+  get 'pages/about', to: 'pages#about'
   devise_for :installs
   resources :listings
   post '/payments/webhook', to: 'payments#webhook'
