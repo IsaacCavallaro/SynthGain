683ca608760021acd07bfb8787e5cba821137c92
isaaccavallaro <isaaccavallaro@gmail.com>
2021-08-02 10:35:03 +1000

    fixed params issue for user profile page

--- app/helpers/listings_helper.rb
+++ app/helpers/listings_helper.rb
@@ -1,16 +1,20 @@
 module ListingsHelper
 
     def render_buy_button
+        #if the user has a login account!
         if current_user
+            #if the user has a login account and DOES have profile/userinfo!
             if has_profile
                 puts "has_profile"
                 button = "<button data-stripe=\"payment\" class=\"btn btn-primary p-3\">Buy: $#{@listing.price}</button>"
             else
+               #if the user has a login account and does NOT have a profile/userinfo
                 puts "no profile"
-                button = link_to  "Buy: $#{@listing.price}", users_info_create_path(current_user.id), class: "btn btn-primary" 
+                button = link_to  "Buy: $#{@listing.price}", users_info_new_path(current_user.id), class: "btn btn-primary" 
             end
             return button.html_safe
         else 
+            #if user does not have a login account! 
             controller.redirect_to new_user_registration_path
         end
     end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
-  get 'users_info/index'
+  get 'users_info/index', to: 'users_info#index', as: 'users_info'
   get 'users_info/show'
   get 'users_info/create'
+  post 'users_info/create', to: 'users_info#create', as: 'users_info_create_post'
   get 'users_info/edit'
   get 'users_info/destroy'
+  get 'users_info/new/:id', to: 'users_info#new', as: 'users_info_new'
   get 'payments/webhook'
   devise_for :users
   root to: 'pages#home'
